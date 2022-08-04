51d54589cc52728d514da4957678293309d062b0
gulskumar <109199121+gulskumar@users.noreply.github.com>
2022-08-04 14:22:03 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,39 @@
+
+variable "team_folder_name" {
+  type = string
+}
+
+variable "first_level_folder_create" {
+  type    = bool
+  default = false
+}
+
+variable "first_level_folder_type" {
+  type    = list(string)
+  default = []
+}
+
+variable "second_level_folder_create" {
+  type    = bool
+  default = false
+}
+
+variable "second_level_folder_type" {
+  type    = list(string)
+  default = []
+}
+
+variable "third_level_folder_create" {
+  type    = bool
+  default = false
+}
+
+variable "third_level_folder_type" {
+  type    = list(string)
+  default = []
+}
+
+
+variable "parent" {
+  type = string
+}
